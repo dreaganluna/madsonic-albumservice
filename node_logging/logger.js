34c499470f1184529979278c5ab6576ca66263c8
mika <Mikael.Spruyt@prosteps.be>
2017-09-05 10:55:29 +0200

    change logger path

--- node_logging/logger.js
+++ node_logging/logger.js
@@ -1,13 +1,13 @@
 var winston = require('winston');
   var logger = function(logName, path) {
-	  path = path || '/node/logs';
+	  path = path || '/nodelogs';
 	var logObject =  new (winston.Logger)({
     transports: [
       new (winston.transports.Console)(),
-      new (winston.transports.File)({ filename: '/node/logs/log'+ logName +'.log', 'level': 'verbose', 'json': false ,handleExceptions: true,    humanReadableUnhandledException: true, maxsize: 5000000, maxFiles: 5, tailable: true})
+      new (winston.transports.File)({ filename: '/nodelogs/log'+ logName +'.log', 'level': 'verbose', 'json': false ,handleExceptions: true,    humanReadableUnhandledException: true, maxsize: 5000000, maxFiles: 5, tailable: true})
     ]
   });
-  
+
 	logObject.remove(winston.transports.Console);
 	logObject.add(winston.transports.Console, {'timestamp':true, 'level': 'verbose', 'colorize':true});
 	return logObject;
